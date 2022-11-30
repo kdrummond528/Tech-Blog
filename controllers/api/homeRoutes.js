89cb397f7670fd81b7e8e8643b5b2150091e0f3b
Karina Drummond <k.drummond528@gmail.com>
2022-11-30 15:06:00 -0800

    modified homeroutes

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,1 +1,25 @@
 const router = require("express").Router();
+
+// route to login
+router.get("/login", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+
+    res.render("login");
+});
+
+// route to sign up
+router.get("/signup", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+
+    res.render("signup");
+});
+
+// get all posts
+
+// get a single post
