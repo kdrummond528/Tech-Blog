6bce15e2f1bea0767b9e8c309670fb5619dec918
Karina Drummond <k.drummond528@gmail.com>
2022-11-30 15:22:29 -0800

    modified index.js

--- models/index.js
+++ models/index.js
@@ -1,15 +1,18 @@
 const User = require('./User');
 const Comment = require('./Comment');
 
+// posts will have many comments from many users
 Post.hasMany(Comment, {
     foreignKey: 'post_id',
     // onDelete: 'CASCADE'
 });
 
+// comments will belong to one user
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+// posts will belong to one user
 Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
