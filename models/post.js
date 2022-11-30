a262d7dd79349b6cd5c3c1ff8903f9885dde3d6a
Karina Drummond <k.drummond528@gmail.com>
2022-11-30 15:25:00 -0800

    modified post.js

---
+++ models/post.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes } = require('sequelize');
+// const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Post extends Model {
+    // checkPassword(loginPw) {
+    //     return bcrypt.compareSync(loginPw, this.password);
+    // }
+}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            // unique: true,
+            references: {
+                // isEmail: true,
+                key: "id",
+                model: "User"
+            },
+        },
+        // post_id: {
+        //     type: DataTypes.INTEGER,
+        //     allowNull: false,
+        //     references: {
+        //         // len: [8],
+        //         key: "id",
+        //         model: "Post"
+        //     },
+        // },
+    },
+    {
+        //     hooks: {
+        //       beforeCreate: async (newUserData) => {
+        //         newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        //         return newUserData;
+        //       },
+        //       beforeUpdate: async (updatedUserData) => {
+        //         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        //         return updatedUserData;
+        //       },
+        //     },
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Post',
+    }
+);
+
+module.exports = Post;
