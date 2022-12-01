a6137e9ad77907eda74e2a8d9e0246bc598aa8a8
Karina Drummond <k.drummond528@gmail.com>
2022-11-30 16:45:46 -0800

    added code to routes and server.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,10 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
-const apiRoutes = require('./homeRoutes.js');
+const homeRoutes = require('./api/homeRoutes');
+router.use('/homeRoutes', apiRoutes);
+
+const homeRoutes = require('./api/homeRoutes');
 router.use('/homeRoutes', apiRoutes);
 
 module.exports = router;
